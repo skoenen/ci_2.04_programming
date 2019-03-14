daaccc4dfce3ff4170475583dcdcc83b44b498ab
Stefan Koenen <sko@hsrw.eu>
2019-03-14 16:10:25 +0100

    ADD: Introduction slides up to "What is a program?"

---
+++ template/assets/app.js
@@ -0,0 +1,48 @@
+//= require reveal/lib/js/head.min.js
+//= require reveal/js/reveal.js
+
+//= require highlight/highlight.pack.js
+
+Reveal.initialize({
+  controlsTutorial: false,
+  history: true,
+  progress: true,
+  fragmentsInURL: true,
+  hideAddressbar: true,
+  overview: true,
+  slideNumber: 'c/t',
+  center: false,
+  dependencies: [
+    { src: '/assets/reveal/lib/js/classList.js', condition: function() {
+        return !document.body.classList;
+      }
+    },
+    { src: '/assets/reveal/plugin/markdown/marked.js', condition: function() {
+        return !!document.querySelector('[data-markdown]');
+      }
+    },
+    { src: '/assets/reveal/plugin/markdown/markdown.js', condition: function() {
+        return !!document.querySelector('[data-markdown]');
+      }
+    },
+    // Speaker notes
+    { src: '/assets/reveal/plugin/notes/notes.js', async: true },
+    // MathJax
+    { src: '/assets/reveal/plugin/math/math.js', async: true }
+  ]
+});
+
+// Change j and k binding for changing slide direction for Colemak
+Reveal.removeKeyBinding( 74 );
+Reveal.addKeyBinding(74, function () { Reveal.up(); });
+Reveal.removeKeyBinding( 75 );
+Reveal.addKeyBinding(75, function () { Reveal.down(); });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block)
+  })
+});
+
+//hjls.initHighlightingOnLoad();
+
